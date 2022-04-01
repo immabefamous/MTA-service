a3eb57568fb519e7500755ce92897d1f7df33fa8
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-04-01 17:18:59 -0400

    added arrival times

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let div  = document.getElementById.apply('container')
+let div  = document.getElementById('container')
 
 const getAllStations = async () => {
     let req = await fetch('https://mtaapi.herokuapp.com/stations')
@@ -8,8 +8,19 @@ const getAllStations = async () => {
        // console.log('Station', element)
        let h2 = document.createElement('h2')
        h2.innerText = element.name
+       h2.dataset.stationId = element.id
+       h2.addEventListener('click', () => {
+        getArrivalTimes(h2.dataset.stationId)
+
+       })
        div.append(h2)
     })
+}
+
+const getArrivalTimes = async () => {
+    let req = await fetch(`https://mtaapi.herokuapp.com/api?id=${stationId}`)
+    let res = await req.json() 
+    console.log('Arrival times', res.result.arrivals)
 }
 
 getAllStations()
